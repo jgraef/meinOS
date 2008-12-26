6ce0bd2a6abed2cc561030b80dfa8b115315de09
Janosch Gräf <janosch@majestix.majestix>
2008-12-26 04:34:33 +0100

    better build/make system

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 make && \
 mkisofs -R -b grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o cdrom.iso files/ && \
-./run_vm.sh && \
-make clean 1>/dev/null 2>/dev/null
+./run_vm.sh
